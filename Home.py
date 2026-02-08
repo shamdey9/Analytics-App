f124361995a41998276011f28a0e4c082262b330
shamdey9 <72339352+shamdey9@users.noreply.github.com>
2026-02-08 17:10:41 +0530

    Home

--- Home.py
+++ Home.py
@@ -1,5 +1,16 @@
 import streamlit as st
+from utils import hide_streamlit_style
 
+hide_streamlit_style()
+hide_streamlit_style = """
+    <style>
+    /* Hide GitHub and Fork icons in Streamlit Cloud header */
+    [data-testid="stToolbar"] {
+        display: none !important;
+    }
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.sidebar.success("Please select below pages")
 
 
